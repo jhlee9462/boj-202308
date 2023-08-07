901d6f0636ebeaa4581225c165d0b8b918572c46
jhlee <jhlee9462@github.com>
2023-08-08 00:19:04 +0900

    complete - chapter 1

---
+++ Cat/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "\\    /\\" << std::endl
+    << " )  ( ')" << std::endl
+    << "(  /  )" << std::endl
+    << " \\(__)|" << std::endl;
+    return 0;
+}
--- ChildJungmin/main.cpp
+++ ChildJungmin/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
+    long a, b, c;
+    std::cin >> a >> b >> c;
+
+    std::cout << a + b + c << std::endl;
 
     return 0;
 }
